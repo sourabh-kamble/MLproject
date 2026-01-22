01e9c5f3537e116da7f1de075c6c3ee9e09795ae
sourabh-kamble <sourabh.kamble.work@gmail.com>
2026-01-22 19:12:14 +0530

    final change

--- app.py
+++ app.py
@@ -20,16 +20,16 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
+        data = CustomData(
+          gender=request.form.get('gender'),
+         race_ethnicity=request.form.get('ethnicity'),
+         parental_level_of_education=request.form.get('parental_level_of_education'),
+         lunch=request.form.get('lunch'),
+         test_preparation_course=request.form.get('test_preparation_course'),
+         reading_score=float(request.form.get('reading_score')),
+         writing_score=float(request.form.get('writing_score'))
         )
+
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
         print("Before Prediction")
